ee43933efd1fa32a0a05cd9630aa8b611bf0d5ba
Dawid Pakuła <zulus@w3des.net>
2015-10-13 11:43:02 +0200

    Fix #130 - Error while opening external file
    Signed-off-by: Dawid Pakuła <zulus@w3des.net>

--- com.dubture.composer.core/src/com/dubture/composer/core/build/BuildParticipantFactory.java
+++ com.dubture.composer.core/src/com/dubture/composer/core/build/BuildParticipantFactory.java
@@ -8,24 +8,25 @@ import org.eclipse.dltk.core.builder.IBuildParticipantFactory;
 import com.dubture.composer.core.ComposerNature;
 
 /**
- * This BuildParticipant  parses the autoload_namespaces.php files to store the namespace information
- * for providing psr-0 Autoloading information (for example in the PEX-Core Class/Interface wizards.)
+ * This BuildParticipant parses the autoload_namespaces.php files to store the
+ * namespace information for providing psr-0 Autoloading information (for
+ * example in the PEX-Core Class/Interface wizards.)
  * 
  * Other than this, it does nothing.
  * 
  * @author Robert Gruendler <r.gruendler@gmail.com>
  */
-public class BuildParticipantFactory implements IBuildParticipantFactory
-{
-    @Override
-    public IBuildParticipant createBuildParticipant(IScriptProject project)
-            throws CoreException
-    {
-        if (project.getProject().hasNature(ComposerNature.NATURE_ID) == false) {
-            return null;
-        }
-        
-        return new ComposerBuildParticipant();
-    }
+public class BuildParticipantFactory implements IBuildParticipantFactory {
+	@Override
+	public IBuildParticipant createBuildParticipant(IScriptProject project) throws CoreException {
+		if (!project.getProject().isAccessible()) {
+			return null;
+		}
+		if (project.getProject().hasNature(ComposerNature.NATURE_ID) == false) {
+			return null;
+		}
+
+		return new ComposerBuildParticipant();
+	}
 
 }
